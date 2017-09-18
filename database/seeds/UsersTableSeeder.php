e5c7c9202cf288725fc188a9e3de466161530b50
Florian Stancioiu <s.florian93@gmail.com>
2017-09-18 10:55:47 +0300

    Create seeder for users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('secret'),
+            'username' => 'john',
+            'show_nsfw' => true,
+            'show_upvote' => true,
+            'gender' => 1,
+            'country' => 'Romania',
+            'birthday_year' => 1990,
+            'birthday_month' => 1,
+            'birthday_day' => 1,
+            'description' => 'A regular John Doe',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Jane Doe',
+            'email' => 'jane@example.com',
+            'password' => bcrypt('secret'),
+            'username' => 'jane',
+            'show_nsfw' => true,
+            'show_upvote' => true,
+            'gender' => 0,
+            'country' => 'Romania',
+            'birthday_year' => 1990,
+            'birthday_month' => 1,
+            'birthday_day' => 1,
+            'description' => 'A regular Jane Doe',
+        ]);
+    }
+}
