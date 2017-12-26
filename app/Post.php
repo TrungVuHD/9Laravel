ec7dfd8bf5491e71bc0ee31200ac6c4e936403a2
Florian Stancioiu <s.florian93@gmail.com>
2017-12-26 12:20:32 +0200

    Add comments to the Post model class

--- app/Post.php
+++ app/Post.php
@@ -3,55 +3,101 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Category;
-use App\Comment;
-use App\Report;
-use App\Point;
-use App\User;
 
 class Post extends Model
 {
+    /**
+     * The array of fillable properties
+     *
+     * @var array
+     */
     protected $fillable = [
-        'title', 'attribution', 'cat_id'
+        'title',
+        'attribution',
+        'cat_id'
     ];
 
+    /**
+     * The Post-Point relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function points()
     {
-
         return $this->hasMany(Point::class);
     }
 
+    /**
+     * The Post-User relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
-
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * The Post-Category relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function category()
     {
-
         return $this->belongsTo(Category::class);
     }
 
+    /**
+     * The Post-Comment relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function comments()
     {
-
         return $this->hasMany(Comment::class);
     }
 
+    /**
+     * The Post-Report relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function reports()
     {
-
         return $this->hasMany(Report::class);
     }
 
+    /**
+     * The next scope function: Post::next()
+     *
+     * @param $query
+     * @param $id
+     * @return mixed
+     */
     public function scopeNext($query, $id)
     {
         return $query->where('id', '>', $id);
     }
 
+    /**
+     * The slug scope function: Post::slug()
+     *
+     * @param $query
+     * @param $slug
+     * @return mixed
+     */
     public function scopeSlug($query, $slug)
     {
         return $query->where('slug', $slug);
+    }
+
+    /**
+     * Return the number of points of a post
+     *
+     * @return int
+     */
+    public function noPoints()
+    {
+        return (int) count($this->points);
     }
 }
