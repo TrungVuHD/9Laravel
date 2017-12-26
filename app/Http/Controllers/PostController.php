0cbb233e2d8bd73bac2b7d47202d523a62e99aef
Florian Stancioiu <s.florian93@gmail.com>
2017-12-26 17:11:51 +0200

    Move the methods prefixed with the word 'retrieve' in the repo class

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\PostStore;
-use Intervention\Image\ImageManagerStatic as Image;
-use grandt\ResizeGif\ResizeGif;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Http\Requests\PostStore;
 use App\Http\Services\PostService;
 use App\Http\Repositories\PostRepository;
 use App\Comment;
@@ -25,21 +22,22 @@ class PostController extends Controller
         $this->service = $service;
     }
 
-    public function index(Request $request)
+    public function index()
     {
-        $posts = $this->retrieveHotAjax(0, 20)['posts'];
+        $posts = $this->repository->retrieveHotAjax(0, 20);
+        //dd($posts->toArray());
         return view('9gag.index', ['posts_category' =>'hot', 'posts' => $posts]);
     }
 
-    public function trendingIndex(Request $request)
+    public function trendingIndex()
     {
-        $posts = $this->retrieveTrendingAjax(0, 20)['posts'];
+        $posts = $this->repository->retrieveTrendingAjax(0, 20)['posts'];
         return view('9gag.index', ['posts_category' =>'trending', 'posts' => $posts]);
     }
 
-    public function freshIndex(Request $request)
+    public function freshIndex()
     {
-        $posts = $this->retrieveFreshAjax(0, 20)['posts'];
+        $posts = $this->repository->retrieveFreshAjax(0, 20)['posts'];
         return view('9gag.index', ['posts_category' =>'fresh', 'posts' => $posts]);
     }
 
@@ -99,180 +97,5 @@ class PostController extends Controller
         $total_results = Post::where('title', 'like', "%$query%")->count();
 
         return view('9gag.search', compact('posts', 'total_results', 'query'));
-    }
-
-    public function retrieveCategoryAjax($category, $start)
-    {
-        $data = ['success' => true];
-        $start = (int)$start;
-        $category = (int)$category;
-
-        try {
-            $data['posts'] = Post::where('cat_id', $category)
-                ->orderBy('id', 'desc')
-                ->offset($start)
-                ->limit(20)
-                ->get();
-
-            foreach ($data['posts'] as &$post) {
-                if ($post->points->where('post_id', $post->id)->where('user_id', Auth::user()->id)->count()) {
-                    $post->active_thumbs_up = true;
-                }
-
-                $post->no_comments = $post->comments->count();
-                $post->no_points = $post->points->count();
-                $post->auth = Auth::check();
-                $post->no_auth = !$post->auth;
-                $post->isnt_gif = !$post->is_gif;
-            }
-        } catch (\Exception $e) {
-            $data['success'] = false;
-        }
-
-        return $data;
-    }
-
-    public function retrieveFreshAjax($offset, $limit)
-    {
-        $offset = (int) $offset;
-        $limit = (int) $limit;
-        $data = ['success' => true];
-
-        try {
-            $data['posts'] = Post::orderBy('id', 'DESC')
-                ->offset($offset)
-                ->limit($limit)
-                ->get();
-
-            foreach ($data['posts'] as &$post) {
-                $post->active_thumbs_up = false;
-
-                if (Auth::check()) {
-                    if ($post->points->where('post_id', $post->id)->where('user_id', Auth::user()->id)->count()) {
-                        $post->active_thumbs_up = true;
-                    }
-                }
-
-                $post->no_comments = $post->comments->count();
-                $post->no_points = $post->points->count();
-                $post->auth = Auth::check();
-                $post->no_auth = !$post->auth;
-                $post->isnt_gif = !$post->is_gif;
-            }
-        } catch (Exception $e) {
-            $data['success']=false;
-        }
-
-        return $data;
-    }
-
-    protected function retrieveTrendingAjax($offset, $limit)
-    {
-
-        $offset = (int) $offset;
-        $limit = (int) $limit;
-        $data = [ 'success' => true ];
-
-        try {
-            // get the first part of posts ids
-            $post_ids = Point::orderBy('post_id', 'desc')
-                ->groupBy('post_id')
-                ->having(DB::raw('count(post_id)'), '>', 300)
-                ->having(DB::raw('count(post_id)'), '<', 1000)
-                ->offset($offset)
-                ->limit($limit)
-                ->pluck('post_id');
-
-            // get the posts
-            $posts = Post::orderBy('id', 'desc')
-                ->whereIn('id', $post_ids)
-                ->get();
-
-            $limit -= $posts->count();
-
-            $more_posts = Post::orderBy('id', 'desc')
-                ->whereNotIn('id', $post_ids)
-                ->offset($offset)
-                ->limit($limit)
-                ->get();
-
-            $posts = $posts->merge($more_posts);
-
-            foreach ($posts as &$post) {
-                $post->active_thumbs_up = false;
-
-                if (Auth::check()) {
-                    if ($post->points->where('post_id', $post->id)->where('user_id', Auth::user()->id)->count()) {
-                        $post->active_thumbs_up = true;
-                    }
-                }
-
-                $post->no_comments = $post->comments->count();
-                $post->no_points = $post->points->count();
-                $post->auth = Auth::check();
-                $post->no_auth = !$post->auth;
-                $post->isnt_gif = !$post->is_gif;
-            }
-
-            $data['posts'] = $posts;
-        } catch (\Exception $e) {
-            $data['success'] = false;
-        }
-
-        return $data;
-    }
-
-    protected function retrieveHotAjax($offset, $limit)
-    {
-
-        $offset = (int) $offset;
-        $limit = (int) $limit;
-        $data = ['success' => true];
-
-        try {
-            // get the first part of posts ids
-            $post_ids = Point::orderBy('post_id', 'desc')
-                ->groupBy('post_id')
-                ->having(DB::raw('count(post_id)'), '>=', 1000)
-                ->offset($offset)
-                ->limit($limit)
-                ->pluck('post_id');
-
-            // get the posts
-            $posts = Post::orderBy('id', 'desc')
-                ->whereIn('id', $post_ids)
-                ->get();
-
-            $limit -= $posts->count();
-
-            $more_posts = Post::orderBy('id', 'desc')
-                ->whereNotIn('id', $post_ids)
-                ->offset($offset)
-                ->limit($limit)
-                ->get();
-
-            $posts = $posts->merge($more_posts);
-
-            foreach ($posts as &$post) {
-                $post->active_thumbs_up = false;
-                if (Auth::check()) {
-                    if ($post->points->where('post_id', $post->id)->where('user_id', Auth::user()->id)->count()) {
-                        $post->active_thumbs_up = true;
-                    }
-                }
-
-                $post->no_comments = $post->comments->count();
-                $post->no_points = $post->points->count();
-                $post->auth = Auth::check();
-                $post->no_auth = !$post->auth;
-                $post->isnt_gif = !$post->is_gif;
-            }
-
-            $data['posts'] = $posts;
-        } catch (\Exception $e) {
-            $data['success'] = false;
-        }
-
-        return $data;
     }
 }
