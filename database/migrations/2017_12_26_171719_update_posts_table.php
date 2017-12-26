3eb9bc4188604b7cfc38adb700769a3c9aa1defd
Florian Stancioiu <s.florian93@gmail.com>
2017-12-26 19:54:14 +0200

    Create migration to update the posts table

---
+++ database/migrations/2017_12_26_171719_update_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->renameColumn('is_gif', 'gif');
+            $table->renameColumn('is_img_huge', 'tall_image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->renameColumn('gif', 'is_gif');
+            $table->renameColumn('tall_image', 'is_img_huge');
+        });
+    }
+}
