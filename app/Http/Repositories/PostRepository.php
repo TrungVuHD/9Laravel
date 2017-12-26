c7cd655451c5756da3c973eecc730ee09e6d3a5f
Florian Stancioiu <s.florian93@gmail.com>
2017-12-26 14:24:39 +0200

    Add the save method to the PostRepository class

--- app/Http/Repositories/PostRepository.php
+++ app/Http/Repositories/PostRepository.php
@@ -2,6 +2,27 @@
 
 namespace App\Http\Repositories;
 
+use App\Post;
+use Illuminate\Support\Facades\Auth;
+
 class PostRepository {
-    //
+    /**
+     *  Persist a post in the database
+     *
+     * @param $data
+     * @return bool
+     */
+    public function save($data)
+    {
+        $nsfw = (int) $data['nsfw'] === 'on';
+        $description = $data['description'];
+        $slug = str_slug($description).'-'.str_random(10);
+
+        $post = new Post($data);
+        $post->slug = $slug;
+        $post->nsfw = $nsfw;
+        $post->user_id = Auth::id();
+
+        return $post->save();
+    }
 }
