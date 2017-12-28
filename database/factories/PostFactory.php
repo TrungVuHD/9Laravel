efbc306803c64644fa1dbb97056d7d27ba544e30
Florian Stancioiu <s.florian93@gmail.com>
2017-12-28 13:27:33 +0200

    Make sure that the migration won't contain any images containing pigs

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -7,7 +7,7 @@ $factory->define(App\Post::class, function (Faker $faker) {
     $title = $faker->sentence(10);
     $slug = str_slug($title);
     $image_dir = storage_path("app/public/" . App\Post::IMG_DIR);
-    $image_categories = [ 'animals', 'city', 'nature', 'cats' ];
+    $image_categories = [ 'technics', 'city', 'nature', 'cats' ];
     $image = $faker->image($image_dir, $width = 1920, $height = 1080, $image_categories[rand(0, 3)]);
     $image_name = basename($image);
 
