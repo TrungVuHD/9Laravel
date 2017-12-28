93a8338417df43b7bf612962e0cdd45c5d702a2d
Florian Stancioiu <s.florian93@gmail.com>
2017-12-28 09:48:53 +0200

    Refactor the CommentController class

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,28 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentIncrementPoint;
+use App\Http\Requests\CommentStore;
+use App\Http\Resources\CommentPointResource;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 use App\CommentPoint;
 use App\Comment;
 
 class CommentController extends Controller
 {
-    public function store(Request $request)
+    /**
+     * Store a record
+     *
+     * @param CommentStore $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(CommentStore $request)
     {
-        if (!Auth::check()) {
-            return redirect('login')
-                ->with('status', 'You need to be logged-in in order to comment.');
-        }
-
-        $this->validate($request, [
-            'comment' => 'required|max:1000',
-            'post_id' => 'required|integer',
-            'parent_id' => 'integer'
-        ]);
-        // add the processed slug variable to the request array
-        $parent_id = (int) $request->parent_id;
-        $request->request->add(['parent_id' => $parent_id]);
         $comment = new Comment($request->all());
         $comment->user_id  = Auth::id();
         $comment->save();
@@ -33,41 +28,34 @@ class CommentController extends Controller
             ->with('status', 'The comment has been posted.');
     }
 
-    public function incrementPoints(Request $request)
+    /**
+     * Increment the point number of a comment
+     *
+     * @param CommentIncrementPoint $request
+     * @return CommentPointResource
+     */
+    public function incrementPoints(CommentIncrementPoint $request)
     {
-        try {
-            $this->validate($request, [
-                'comment_id' => 'required|integer'
-            ]);
+        $point = new CommentPoint();
+        $point->comment_id = $request->comment_id;
+        $point->user_id = Auth::id();
+        $point->save();
 
-            $point = new CommentPoint();
-            $point->comment_id = $request->comment_id;
-            $point->user_id = Auth::user()->id;
-
-            $point->save();
-        } catch (\Exception $e) {
-            return ['success' => false];
-        }
-
-        return ['success' => true];
+        return new CommentPointResource($point);
     }
 
-    public function decrementPoints(Request $request)
+    /**
+     * Decrement the point number of a comment
+     *
+     * @param CommentIncrementPoint $request
+     * @return CommentPointResource
+     */
+    public function decrementPoints(CommentIncrementPoint $request)
     {
-        try {
-            $this->validate($request, [
-                'comment_id' => 'required|integer'
-            ]);
+        $point = CommentPoint::where('comment_id', $request->comment_id)
+            ->where('user_id', Auth::id())
+            ->delete();
 
-            $point = CommentPoint::where('comment_id', $request->comment_id)
-                ->where('user_id', Auth::user()->id)
-                ->first();
-
-            $point->delete();
-        } catch (\Exception $e) {
-            return ['success' => false];
-        }
-
-        return ['success' => true];
+        return new CommentPointResource($point);
     }
 }
---
+++ app/Http/Resources/CommentPointResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class CommentPointResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'comment_id' => $this->post_id,
+            'user_id' => $this->user_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
