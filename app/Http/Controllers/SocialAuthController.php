1fe00df78c37e9e10e4215e8beed386f88a223fc
Florian Stancioiu <s.florian93@gmail.com>
2017-12-28 10:35:19 +0200

    Refactor the SocialAuthController

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -2,41 +2,58 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests;
 use App\SocialAccountService;
-use App\Http\Controllers\Controller;
-use Socialite;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialAuthController extends Controller
 {
+    /**
+     * Redirect the Facebook request
+     *
+     * @return mixed
+     */
     public function facebookRedirect()
     {
         return Socialite::driver('facebook')->redirect();
     }
 
+    /**
+     * Redirect the Google request
+     *
+     * @return mixed
+     */
     public function googleRedirect()
     {
         return Socialite::driver('google')->redirect();
     }
 
+    /**
+     * Authenticate Facebook users
+     *
+     * @param SocialAccountService $service
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function facebookCallback(SocialAccountService $service)
     {
         $provider = Socialite::driver('facebook')->user();
         $user = $service->createOrGetUser($provider, 'facebook');
-        $logged = Auth::login($user);
-        $is_logged = Auth::check();
+        Auth::login($user);
 
         return redirect('/');
     }
 
+    /**
+     * Authenticate Google users
+     *
+     * @param SocialAccountService $service
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function googleCallback(SocialAccountService $service)
     {
         $provider = Socialite::driver('google')->user();
         $user = $service->createOrGetUser($provider, 'google');
-        $logged = Auth::login($user);
-        $is_logged = Auth::check();
+        Auth::login($user);
 
         return redirect('/');
     }
