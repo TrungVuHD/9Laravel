2144344a80ec91874ffe0435420485ed7ebe8b0a
Florian Stancioiu <s.florian93@gmail.com>
2017-12-28 11:08:32 +0200

    Refactor the CategoriesComposer class

--- app/Http/ViewComposers/CategoriesComposer.php
+++ app/Http/ViewComposers/CategoriesComposer.php
@@ -1,26 +1,22 @@
-<?php 
+<?php
 
 namespace App\Http\ViewComposers;
 
 use Illuminate\View\View;
-use Illuminate\Http\Request;
 use App\Category;
 
-
-class CategoriesComposer 
+class CategoriesComposer
 {
+    /**
+     * Add the categories variables to every single view
+     *
+     * @param View $view
+     */
+    public function compose(View $view)
+    {
+        $menu_categories = Category::where('published', 1)->get();
+        $menu_visible_categories = Category::where('show_in_menu', 1)->where('published', 1)->get();
 
-	protected $categories;
-	protected $menuCategories;
-
-	public function compose(View $view) 
-	{	
-		$this->categories = Category::where('published', 1)->get();
-		$this->menuCategories = Category::where('show_in_menu', 1)->where('published', 1)->get();
-
-		$view->with([
-			'menuPostCategories' => $this->categories,
-			'menuVisiblePostCategories' => $this->menuCategories,
-		]);
-	}
+        $view->with(compact('menu_categories', 'menu_visible_categories'));
+    }
 }
